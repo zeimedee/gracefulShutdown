ce97c4093770e5fcbae7d85579d61c8c7494f815
zeimedee <42755623+zeimedee@users.noreply.github.com>
2023-01-13 10:04:26 +0000

    golang graceful shutdown

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func createChannel() (chan os.Signal, func()) {
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	return stopCh, func() {
+		close(stopCh)
+	}
+}
+
+func start(server *http.Server) {
+	log.Println("application started")
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		panic(err)
+	} else {
+		log.Println("application stopped gracefully")
+	}
+}
+
+func shutDown(ctx context.Context, server *http.Server) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		panic(err)
+	} else {
+		log.Panicln("application shutdowned")
+	}
+
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	s := &http.Server{}
+
+	go start(s)
+
+	stopCh, closeCh := createChannel()
+	defer closeCh()
+	log.Println("notified", <-stopCh)
+
+	shutDown(context.Background(), s)
+}
